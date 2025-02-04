f2f44bd19f7eba0222d681fc498c5febbfb473a5
Jinwoo Sung <jwsung91@gmail.com>
2025-02-04 12:58:36 +0900

    refactor: apply clang-format

--- dijkstra/dijkstra.cc
+++ dijkstra/dijkstra.cc
@@ -1,36 +1,32 @@
 #include "dijkstra.hpp"
 
-vector<int> Dijkstra::shortest_path(int start)
-{
-    vector<int> dist(n, INF);
-    dist[start] = 0;
+vector<int> Dijkstra::shortest_path(int start) {
+  vector<int> dist(n, std::numeric_limits<int>::max());
+  dist[start] = 0;
 
-    priority_queue<Node> pq;
-    pq.push({start, 0});
+  priority_queue<Node> pq;
+  pq.push({start, 0});
 
-    while (!pq.empty())
-    {
-        Node current = pq.top();
-        pq.pop();
+  while (!pq.empty()) {
+    Node current = pq.top();
+    pq.pop();
 
-        int current_vertex = current.vertex;
-        int current_cost = current.cost;
+    int current_vertex = current.vertex;
+    int current_cost = current.cost;
 
-        if (current_cost > dist[current_vertex])
-            continue;
+    if (current_cost > dist[current_vertex])
+      continue;
 
-        for (const Edge &edge : graph[current_vertex])
-        {
-            int next_vertex = edge.to;
-            int next_cost = current_cost + edge.weight;
+    for (const Edge &edge : graph[current_vertex]) {
+      int next_vertex = edge.to;
+      int next_cost = current_cost + edge.weight;
 
-            if (next_cost < dist[next_vertex])
-            {
-                dist[next_vertex] = next_cost;
-                pq.push({next_vertex, next_cost});
-            }
-        }
+      if (next_cost < dist[next_vertex]) {
+        dist[next_vertex] = next_cost;
+        pq.push({next_vertex, next_cost});
+      }
     }
+  }
 
-    return dist;
+  return dist;
 }
--- dijkstra/dijkstra.hpp
+++ dijkstra/dijkstra.hpp
@@ -1,34 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 #include <limits>
+#include <queue>
+#include <vector>
 
 using namespace std;
 
-const int INF = numeric_limits<int>::max();
-
-struct Edge
-{
-    int to;
-    int weight;
+struct Edge {
+  int to;
+  int weight;
 };
 
-class Dijkstra
-{
+class Dijkstra {
 public:
-    Dijkstra(int n, const vector<vector<Edge>> &graph) : n(n), graph(graph) {}
-    vector<int> shortest_path(int start);
+  Dijkstra(int n, const vector<vector<Edge>> &graph) : n(n), graph(graph) {}
+  vector<int> shortest_path(int start);
 
 private:
-    struct Node
-    {
-        int vertex;
-        int cost;
-        bool operator<(const Node &other) const
-        {
-            return cost > other.cost;
-        }
-    };
-    int n;
-    const vector<vector<Edge>> &graph;
+  struct Node {
+    int vertex;
+    int cost;
+    bool operator<(const Node &other) const { return cost > other.cost; }
+  };
+  int n;
+  const vector<vector<Edge>> &graph;
 };
