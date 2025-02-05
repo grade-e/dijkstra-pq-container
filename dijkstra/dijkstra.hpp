76cf7f9a8aff1d21f3acb86fe110b285684b36c9
Jinwoo Sung <123284056+jwsung91@users.noreply.github.com>
2025-02-05 09:43:52 +0900

    refactor: dijkstra class (#3)
    Signed-off-by: Jinwoo Sung <jwsung91@gmail.com>

--- dijkstra/dijkstra.hpp
+++ dijkstra/dijkstra.hpp
@@ -1,26 +1,28 @@
 #include <iostream>
-#include <limits>
-#include <queue>
 #include <vector>
+#include <queue>
+#include <limits>
+#include <utility>
 
 using namespace std;
 
-struct Edge {
-  int to;
-  int weight;
+using Edge = pair<int, int>; // {destination, weight}
+
+struct Node
+{
+  int vertex;
+  int cost;
+  bool operator>(const Node &np) const { return this->cost > np.cost; }
 };
 
-class Dijkstra {
+class Dijkstra
+{
 public:
   Dijkstra(int n, const vector<vector<Edge>> &graph) : n(n), graph(graph) {}
-  vector<int> shortest_path(int start);
+  vector<int> shortest_paths(int start);
+  int shortest_path(int start, int target);
 
 private:
-  struct Node {
-    int vertex;
-    int cost;
-    bool operator<(const Node &other) const { return cost > other.cost; }
-  };
   int n;
   const vector<vector<Edge>> &graph;
 };
